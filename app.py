1f949c092770a7d1d9550bfdbcd89ab5098f1a72
Infiark_air1 <Infiark_air1@Infiark-air1s-MacBook-Air.local>
2018-08-15 17:14:43 +0800

    add run.py

--- app.py
+++ app.py
@@ -15,10 +15,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.secret_key = 'hughe'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 # JWT會創造一個新的端點/auth，當用戶發送username及password到/auth時，
 # JWT會使用它們，透過authenticate()來驗證並發送JWT Token，並在之後的request中
 # 透過identity()調用符合身分的用戶。
